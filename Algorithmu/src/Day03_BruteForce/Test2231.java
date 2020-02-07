117f2db58691aa218610a6815a395316ddacb71c
leefree3 <queen.minhee@gmail.com>
2020-02-07 16:31:16 +0900

    BruteForce 백준 확인 후

--- Algorithmu/src/Day03_BruteForce/Test2231.java
+++ Algorithmu/src/Day03_BruteForce/Test2231.java
@@ -3,7 +3,7 @@ package Day03_BruteForce;
 import java.util.Scanner;
 
 public class Test2231 {
-
+	//Åë°ú ¾ÈµÊ... Ã£¾ÆºÁ¾ß°Ú´Ù..
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
